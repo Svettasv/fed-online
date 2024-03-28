00384f4e8170cb98ca910ea9f40f2604b950a1f8
Svitlana <atevssv@gmail.com>
2024-03-28 21:39:36 +0200

    JS HW 01

---
+++ JS_01/main.js
@@ -0,0 +1,9 @@
+// Оголосіть дві змінні: userAgeValue з числовим значенням та userNameNickname з значенням текстовим.
+
+let userAgeValue = 38;
+let userNameNickname = "Oleg";
+
+//Змініть значення userAgeValue так задайте нове значення userNameNickname
+
+userAgeValue = 25;
+userNameNickname = "Dmytro";
