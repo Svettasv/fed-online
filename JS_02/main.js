2a9c71648eca3b9d5c0f6b31b370de17bbe33bed
Svitlana <atevssv@gmail.com>
2024-03-28 22:04:02 +0200

    JS HW 02

---
+++ JS_02/main.js
@@ -0,0 +1,39 @@
+// Зробіть розрахунки і виведіть в консоль ось такі математичні операції
+
+// let result1 = (5 + 3) * 2 - 7;
+
+// let result2 = (10 / 2) ** 2;
+
+// let result3 = 2 * (4 + 3) - (8 / 2);
+
+// let result4 = (15 % 4) + 5;
+
+// let result5 = 3 - (2 ** 4);
+
+let result1 = (5 + 3) * 2 - 7;
+
+let result2 = (10 / 2) ** 2;
+
+let result3 = 2 * (4 + 3) - 8 / 2;
+
+let result4 = (15 % 4) + 5;
+
+let result5 = 3 - 2 ** 4;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+
+// Також свторіть 2 змінні і самостійно виведіть результат операцій +,-,*,/,%,**
+
+let firstNumber = 10;
+let secondNumber = 5;
+
+console.log(`10 + 5 = ${firstNumber + secondNumber}`);
+console.log(`10 - 5 = ${firstNumber - secondNumber}`);
+console.log(`10 * 5 = ${firstNumber * secondNumber}`);
+console.log(`10 / 5 = ${firstNumber / secondNumber}`);
+console.log(`10 % 5 = ${firstNumber % secondNumber}`);
+console.log(`10 ** 5 = ${firstNumber ** secondNumber}`);
