75cdfe38fed51f182978064da439b16f2b41c0b9
Svitlana <atevssv@gmail.com>
2024-05-09 18:46:32 +0300

    timer code changes

--- JS_20/main2.js
+++ JS_20/main2.js
@@ -13,28 +13,21 @@ window.onload = function () {
     secondsId.innerHTML =
       counterSeconds < 10 ? "0" + counterSeconds : counterSeconds;
     if (counterSeconds === 0) {
-      clearInterval(intervalID);
-      secondsId.innerHTML = "00";
+      counterMinutes--;
+      minutesId.innerHTML =
+        counterMinutes < 10 ? "0" + counterMinutes : counterMinutes;
+      if (counterMinutes < 0) {
+        clearInterval(intervalID);
+        clearInterval(intervalMinId);
+        secondsId.innerHTML = "00";
+        minutesId.innerHTML = "00";
+      }
       counterSeconds = 60;
-      intervalID = setInterval(countSec, 1000);
-    }
-  }
-
-  function countMin() {
-    counterMinutes--;
-    minutesId.innerHTML =
-      counterMinutes < 10 ? "0" + counterMinutes : counterMinutes;
-    if (counterMinutes < 0) {
-      clearInterval(intervalID);
-      clearInterval(intervalMinId);
-      secondsId.innerHTML = "00";
-      minutesId.innerHTML = "00";
     }
   }
 
   startBtn.addEventListener("click", function () {
     intervalID = setInterval(countSec, 1000);
-    intervalMinId = setInterval(countMin, 60000);
   });
 
   resetBtn.addEventListener("click", function () {
